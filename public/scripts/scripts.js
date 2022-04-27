4f7fdc26e644ff8a64c22dcaec08e8b37c1466c6
cristhian moura <cristhian_moura@live.com>
2022-04-26 23:18:19 -0300

    Versão 1.0
    Montagem  do layout da loja bloom modas

---
+++ public/scripts/scripts.js
@@ -0,0 +1,18 @@
+function msg() {
+    alert("Button is ok!!");
+}
+function mostrar() {
+    document.getElementById("dropopt").classList.toggle('show');
+}
+window.onclick = (event) => {
+    if (!event.target.matches('.dropcat')) {
+
+        var dropcate = document.getElementsByClassName("dropopt");
+        for (let i = 0; i < dropcate.length; i++) {
+            var opendrop = dropcate[i];
+            if (opendrop.classList.contains('show')) {
+                opendrop.classList.remove('show');
+            }
+        }
+    }
+}
